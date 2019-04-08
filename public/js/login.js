b5a22f422b0dfdc37f6d398b0e7c81c5d8977610
MSI\jeff <hatcher28@me.com>
2019-04-08 15:38:32 -0700

    login

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+  $("#login").on("click", function (e) {
+
+    alert('login clicked')
+  })
+    
+
+});
+
+
+
